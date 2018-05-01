166fb3b21b30ca0e1b2f9e0ef41da9ef3367813a
zer0ttl <sudheer.shirsath+zer0ttl@gmail.com>
2018-05-01 13:01:18 +0530

    integrated unittest into functional_tests.py. this can be used as boilerplate for functional_tests.

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,18 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+class HomePageTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
